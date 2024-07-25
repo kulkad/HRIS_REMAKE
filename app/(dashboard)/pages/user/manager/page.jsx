a3fb5832e51459cbfe29e586d464d5773266aad4
ryunex15 <badarudin1511@gmail.com>
2024-07-25 11:45:37 +0700

    maju hitam

--- app/(dashboard)/pages/user/manager/page.jsx
+++ app/(dashboard)/pages/user/manager/page.jsx
@@ -5,34 +5,19 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import axios from "axios";
 import Link from "next/link";
-// import node module libraries
 import { Fragment } from "react";
 import { Container, Col, Row } from "react-bootstrap";
-
-// import widget/custom components
-import { StatRightTopIcon } from "widgets";
-
-// import sub components
-import { ActiveProjects, Teams, TasksPerformance } from "sub-components";
-
-// import required data files
-import ProjectsStatsData from "data/dashboard/ProjectsStatsData";
-import {
-  EmojiSmile,
-  PersonFillGear,
-  PersonVcard,
-  TrashFill,
-} from "react-bootstrap-icons";
+import { EmojiSmile, PersonVcard, TrashFill } from "react-bootstrap-icons";
 
 const Manager = () => {
   const [user, setUser] = useState(null);
   const [usersByRole, setUsersByRole] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [usersPerPage] = useState(5);
-  const [successMessage, setSuccessMessage] = useState(""); // State for success message
+  const [successMessage, setSuccessMessage] = useState("");
   const role = "Manager";
-  const [showModal, setShowModal] = useState(false); // State for modal visibility
-  const [userToDelete, setUserToDelete] = useState(null); // State for the user to delete
+  const [showModal, setShowModal] = useState(false);
+  const [userToDelete, setUserToDelete] = useState(null);
 
   const openDeleteModal = (id) => {
     setUserToDelete(id);
@@ -94,15 +79,7 @@ const Manager = () => {
     }
   };
 
-  const toggleDropdown = (id) => {
-    setOpenDropdown((prevOpenDropdown) =>
-      prevOpenDropdown === id ? null : id
-    );
-  };
-
-  const closeDropdownHandler = () => {
-    setOpenDropdown(null);
-  };
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   if (!user) {
     return (
@@ -116,13 +93,9 @@ const Manager = () => {
     );
   }
 
-  // Calculating the users to be displayed on the current page
   const indexOfLastUser = currentPage * usersPerPage;
   const indexOfFirstUser = indexOfLastUser - usersPerPage;
   const currentUsers = usersByRole.slice(indexOfFirstUser, indexOfLastUser);
-
-  // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <Fragment>
@@ -130,99 +103,87 @@ const Manager = () => {
       <Container fluid className="mt-n22 px-6">
         <Row>
           <Col lg={12} md={12} xs={12}>
-            {/* Page header */}
-            <div>
-              <div className="d-flex justify-content-between align-items-center mx-5">
-                <div className="mb-2 mb-lg-0">
-                  <h3 className="mb-0  text-white">Data Manager</h3>
-                </div>
-                <div>
-                  <Link
-                    href="/pages/user/tambah/?role=Manager"
-                    className="btn btn-white"
-                  >
-                    Tambah Data
-                  </Link>
-                </div>
+            <div className="d-flex justify-content-between align-items-center mx-5">
+              <div className="mb-2 mb-lg-0">
+                <h3 className="mb-0 text-white">Data Manager</h3>
+              </div>
+              <div>
+                <Link
+                  href="/pages/user/tambah/?role=Manager"
+                  className="btn btn-white"
+                >
+                  Tambah Data
+                </Link>
               </div>
             </div>
           </Col>
-          <div className="container bg-white dark:bg-slate-900 dark:text-white my-5 p-4 rounded shadow">
-            {successMessage && (
-              <p className="text-green-600">{successMessage}</p>
-            )}
-            {/* Tampilkan pesan jika ada */}
-            <div className="">
+          <Col lg={12} md={12} xs={12}>
+            <div className="bg-white dark:bg-slate-900 dark:text-white my-5 p-4 rounded shadow">
+              {successMessage && (
+                <p className="text-green-600">{successMessage}</p>
+              )}
               {usersByRole.length === 0 ? (
                 <p className="text-center py-4">Tidak ada data</p>
               ) : (
-                <table className="w-full text-sm text-left text-gray-500 dark:bg-slate-900 dark:text-white ">
-                  <thead className="text-xs text-gray-900 uppercase">
-                    <tr>
-                      <th className="px-6 py-3">Nama</th>
-                      <th className="px-6 py-3">Email</th>
-                      <th className="px-6 py-3">Role</th>
-                      <th className="px-6 py-3"></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {currentUsers.map((user) => (
-                      <tr key={user.id} className="bg-white dark:bg-gray-800">
-                        <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          {user.name}
-                        </th>
-                        <td className="px-6 py-4">{user.email}</td>
-                        <td className="px-6 py-4">{user.role}</td>
-                        <td className="px-6 py-4"> 
-                          
-                          <Link
-                            href={`/pages/user/detailuser/${user.id}`}
-                            className="d-flex items-center hover:bg-blue-200 hover:text-gray-800 rounded-xl p-2 w-full"
-                          >
-                            <PersonVcard className="mt-1 me-2" /> Detail
-                          </Link>
-                       
-                          <Link
-                            key={user.id}
-                            href={`/register/${user.id}?role=${user.role}`}
-                            className="d-flex items-center hover:bg-blue-200 hover:text-gray-800 rounded-xl p-2 w-full"
-                          >
-                            {user.url_foto_absen == null ? (
-                              <>
-                                <EmojiSmile className="mt-1 me-2" /> Daftar Muka
-                              </>
-                            ) : (
-                              <span>Muka Sudah Terdaftar</span>
-                            )}
-                          </Link>
-                          <button
-                          onClick={() => {
-                            openDeleteModal(user.id);
-                            closeDropdownHandler();
-                          }}
-                          className="d-flex items-center hover:bg-red-300 hover:text-gray-800 rounded-xl p-2 w-full"
-                        >
-                          <TrashFill className="mt-1 me-2" /> Delete
-                        </button>
-                     
-                        </td>
+                <div className="table-responsive d-flex justify-content-center">
+                  <table className="table w-auto text-sm text-left text-gray-500 dark:bg-slate-900 dark:text-white">
+                    <thead className="text-xs text-gray-900 uppercase bg-gray-200 dark:bg-gray-700">
+                      <tr>
+                        <th className="px-6 py-3 text-center">Nama</th>
+                        <th className="px-6 py-3 text-center">Email</th>
+                        <th className="px-6 py-3 text-center">Role</th>
+                        <th className="px-6 py-3 text-center">Aksi</th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
+                    </thead>
+                    <tbody>
+                      {currentUsers.map((user) => (
+                        <tr key={user.id} className="bg-white dark:bg-gray-800">
+                          <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
+                            {user.name}
+                          </td>
+                          <td className="px-6 py-4 text-center">{user.email}</td>
+                          <td className="px-6 py-4 text-center">{user.role}</td>
+                          <td className="px-6 py-4 flex justify-end gap-2 text-center">
+                            <Link
+                              href={`/pages/user/detailuser/${user.id}`}
+                              className="d-flex items-center hover:bg-blue-200 hover:text-gray-800 rounded-xl p-2"
+                            >
+                              <PersonVcard className="mt-1 me-2" /> Detail
+                            </Link>
+                            <Link
+                              href={`/register/${user.id}?role=${user.role}`}
+                              className="d-flex items-center hover:bg-blue-200 hover:text-gray-800 rounded-xl p-2"
+                            >
+                              {user.url_foto_absen == null ? (
+                                <>
+                                  <EmojiSmile className="mt-1 me-2" /> Daftar Muka
+                                </>
+                              ) : (
+                                <span>Muka Sudah Terdaftar</span>
+                              )}
+                            </Link>
+                            <button
+                              onClick={() => openDeleteModal(user.id)}
+                              className="d-flex items-center hover:bg-red-300 hover:text-gray-800 rounded-xl p-2"
+                            >
+                              <TrashFill className="mt-1 me-2" /> Delete
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
               )}
-            </div>
-              {/* Pagination Controls */}
               <div className="d-flex justify-content-center mt-4">
                 <ul className="inline-flex items-center -space-x-px">
                   {[
-                    ...Array(
-                      Math.ceil(usersByRole.length / usersPerPage)
-                    ).keys(),
+                    ...Array(Math.ceil(usersByRole.length / usersPerPage)).keys(),
                   ].map((number) => (
                     <button
+                      key={number}
                       onClick={() => paginate(number + 1)}
-                      className={`px-3 py-2 leading-tight text-gray-500 bg-gray-100 border border-gray-300 hover:bg-sky-100 bg-primary-hover rounded-2 dark:hover:bg-sky-200 dark:bg-gray-50 ${
+                      className={`px-3 py-2 leading-tight text-gray-500 bg-gray-100 border border-gray-300 hover:bg-sky-100 rounded-2 dark:hover:bg-sky-200 dark:bg-gray-50 ${
                         currentPage === number + 1 ? "bg-gray-300" : ""
                       }`}
                     >
@@ -231,10 +192,10 @@ const Manager = () => {
                   ))}
                 </ul>
               </div>
-          </div>
+            </div>
+          </Col>
         </Row>
       </Container>
-           
     </Fragment>
   );
 };
