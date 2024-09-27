87cf3fa83897701cf6f6369cf3719f57eeb12da0
ahmd-fariz <ahmadfarizmaulana4@gmail.com>
2024-09-27 15:24:51 +0700

    ganti domain

--- app/api/hello.js
+++ app/api/hello.js
@@ -1,2 +1,2 @@
-export const API_Frontend = "http://89.116.187.91:3001"
-export const API_Backend = "http://89.116.187.91:5001"
+export const API_Frontend = "https://hris.grageweb.online/"
+export const API_Backend = "https://apihris.grageweb.online/"
