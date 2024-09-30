eec470d7c4587a38d52570c99ebc2193ee7334dd
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-09-30 10:49:20 +0700

    coba

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Mengabaikan error ESLint selama proses build
+    '@next/next/no-img-element': 'off', // Menonaktifkan aturan ini di seluruh proyek
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
