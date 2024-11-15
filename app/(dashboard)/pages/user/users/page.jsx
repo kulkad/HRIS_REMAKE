cbae25814504357fa504b41fbcf8013cbc185abb
Baihaqi34 <abay999mmm@gmail.com>
2024-11-15 10:28:27 +0700

    loading users

--- app/(dashboard)/pages/user/users/page.jsx
+++ app/(dashboard)/pages/user/users/page.jsx
@@ -20,6 +20,7 @@ import {
 import { EmojiSmile, PersonVcard, TrashFill } from "react-bootstrap-icons";
 import { FaEllipsisVertical } from "react-icons/fa6";
 import Swal from "sweetalert2";
+import Skeleton from 'react-loading-skeleton';
 import { API_Frontend, API_Backend } from "../../../../api/hello.js";
 
 // Fungsi untuk menghitung luminance
@@ -49,7 +50,7 @@ const Users = () => {
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [loading, setLoading] = useState("");
+  const [loading, setLoading] = useState(true);
   const [password, setPassword] = useState("");
   const [confPassword, setConfPassword] = useState("");
   const [file, setFile] = useState("");
@@ -195,6 +196,8 @@ const Users = () => {
       setFilteredUsers(response.data); // Inisialisasi dengan semua pengguna
     } catch (error) {
       console.error("Error fetching users:", error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -251,6 +254,18 @@ const Users = () => {
       });
     }
   };
+
+  if (loading) {
+    return (
+      <div className="container bg-white dark:bg-slate-900 dark:text-white my-5 p-4 rounded shadow">
+        <Skeleton height={40} count={1} className="mb-4" />
+        <Skeleton height={20} count={1} className="mb-4" />
+        <Skeleton height={20} count={1} className="mb-4" />
+        <Skeleton height={50} width={150} className="mb-4" />
+        <Skeleton height={50} width={150} className="mb-4" />
+      </div>
+    );
+  }
 
   if (!user) {
     return (
